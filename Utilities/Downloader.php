e299a46e6733e81a10fe8fe894ac2970ae93e094
blaine@millers.cloud <millers@localhost>
2023-02-02 20:01:30 -0800

    fix: windows installer and blocking reliability
    Summary:
    The first attempt at a windows installer had majors issues that resulted
    in windows failing to resolve any dns responses and locked the user from
    fixing the issues easily. This changed the windows script to use other
    open source software that provides a far more stable dns resolution in
    windows as a service. The new script will automatically setup the
    necessary tools in windows for you. This brings in adguards dnsproxy
    (apache license 2.0) and NSSM (public domain) windows binaries so that
    they can be used to setup CalypsDoH on windows. The other major fix is
    how annoyances and alarmables are matched. Previously, the grep
    implementation would do partial matches which resulted in false
    positives and over blocking. The change fixes the grep calls to match
    exact domains against the blocklists.
    
    Test Plan:
    used dnsleaktest.org for its web based doh testing interface to ensure
    that both sites that should be blocked are and that a known false
    positive is no longer blocked.

--- Utilities/Downloader.php
+++ Utilities/Downloader.php
@@ -6,6 +6,9 @@ use CalypsDoH\Utilities\UUID;
 
 class Downloader {
     function __construct(string $identity, string $deviceName) {
+        if($_GET['dl'] === 'windows') {
+            self::downloadWindowsInstaller($identity, $deviceName);
+        }
         self::downloadAppleProfile($identity, $deviceName);
     }
 
@@ -64,6 +67,76 @@ class Downloader {
             <integer>1</integer>
             </dict>
             </plist>';
+        die();
+    }
+
+    public static function downloadWindowsInstaller(string $identity, string $deviceName) {
+        header('Content-Type: application/bat');
+        header('Content-Disposition: attachment; filename="barker-windows-'
+                                    .$deviceName.'.bat"'); 
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+
+        flush(); 
+
+        echo '@echo off
+SET scriptpath=%~dp0
+call :isAdmin
+
+if %errorlevel% == 0 (
+    goto :run
+) else (
+    echo Requesting administrative privileges...
+    goto :UACPrompt
+)
+
+exit /b
+
+:isAdmin
+    fsutil dirty query %systemdrive% >nul
+exit /b
+
+:run
+    set DoHClientAddress=https://' . $_SERVER['HTTP_HOST'] . '/' . $identity . '/' . rawurlencode($deviceName) . '
+    
+    curl.exe --output C:\nssm.exe --url https://barker.millers.cloud/CalypsDoH/Installers/Windows/nssm.exe
+    curl.exe --output C:\dnsproxy.exe --url https://barker.millers.cloud/CalypsDoH/Installers/Windows/dnsproxy.exe
+    net stop Barker
+    C:\nssm.exe remove Barker
+    C:\nssm.exe install Barker "C:\dnsproxy.exe" "-l 0.0.0.0 -p 53 -u %DoHClientAddress% -b 1.1.1.1:53"
+    net start Barker
+
+    rem The following for loops get a given interface\'s InterfaceIndex and GUID. We use the InterfaceIndex to set DNS, and the GUID to set DoH in the registry.
+    rem We only care about network interfaces that have a GUID.
+    for /f %%X in (\'wmic nic where "GUID!=NULL" Get InterfaceIndex /value\') do (
+        rem We have to use a second for loop to remove the extra carrige returns from wmic output.
+        rem InterfaceIndex is stored at %%I.
+        for /f "tokens=1* delims==" %%H in ("%%X") do (
+            for /f %%X in (\'wmic nic where "InterfaceIndex=%%I" Get GUID /value\') do (
+                rem GUID is stored at %%G.
+                for /f "tokens=1* delims==" %%F in ("%%X") do (
+                    rem Clears existing DNS servers.
+                    netsh interface ipv4 set dnsservers %%I dhcp 1>NUL
+                    netsh interface ipv6 set dnsservers %%I dhcp 1>NUL
+                    rem Use Local Service for DNS Server
+                    netsh interface ipv4 set dnsservers %%I static 127.0.0.1 primary no 1>NUL
+                    netsh interface ipv6 set dnsservers %%I static :: primary no 1>NUL
+                )
+            )
+        )
+    )
+
+    ipconfig /flushdns 1>NUL
+exit /b
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B`';
         die();
     }
 }
